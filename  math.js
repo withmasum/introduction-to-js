dda5e7b495aab86fe2b936d830d92de820a1d391
Masum Ahmed <creatormasum@gmail.com>
2022-08-01 02:47:10 +0600

    first commit

---
+++  math.js
@@ -0,0 +1,22 @@
+/*** 
+
+1. Number =>> 55. 898, -32
+2. integer, odd, even: 2, 5, 
+3. float: decimal: 2.5, 13,67 32, 69
+3. operation: addition substraction multiplication division reminder 
+5. percent, 
+6. average 
+7. comparison: >, <, ==, !=. >=. <= 
+8. Oikik : 2 kilo apple and 5 kilo angur: 
+9. 45, 25, 68, 129, 2: max, min, avg, sum
+10. series: 
+
+::: EXTRA ::: 
+
+1. Prime 
+2. Permutation
+3. Area 
+::: ALWAYS ::: Jodi kisu lage shikhe nite hobeh 
+
+*/
+
