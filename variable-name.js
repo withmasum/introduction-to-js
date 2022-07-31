5796dfed1f89dbecfe712137d72f8213f8e08357
Masum Ahmed <creatormasum@gmail.com>
2022-08-01 03:45:46 +0600

    variable name js

---
+++ variable-name.js
@@ -0,0 +1,31 @@
+var price = 29;
+
+// 1. Variable name can not be any keywords 
+
+var false = 96;
+var return = true;
+
+
+// 2. Variable name has to be in one word. No space allowed. 
+var my home address = "New California";
+
+// 3. var name can not have quotation 
+var "name" = "Tom Hanks";
+
+// 4. variable name can not strats with a number but can ends with a number
+
+var 99Club = 19664;
+var club25 = 2025;
+
+// 5. How to use long names
+// can not use dash 
+
+var user-name = "bappa raj";
+
+var user_name = "bappa raj";
+
+
+var user address = "andor killa bandor ban"
+var user_current_address = "andor killa bandor ban" //snake case
+var userHomeAddress = "andor killa bandor ban" //camel case  we will use this one 
+var UserHomeAddress = "andor killa bandor ban" //pascal case 
--- variable.js
+++ variable.js
@@ -2,7 +2,9 @@
 // String 
 // Numeric 
 
+// So variable type is numeric,  string, bollean 
 
+// numeric 
 var price = 13;
 var age = 17;
 var temparature = 38;
@@ -10,9 +12,18 @@ var weight = 37;
 var money = 4000;
 var sex = 999;
 
+
+// string 
 var person = "sodor uddin";
 var location = "andork killa bandorbon";
 var special = 'alia bhatt';
+
+
+
+// Boolean 
+var serious = true;
+var ifFullMarks = true;
+var isSingle = false;
 
 
 
