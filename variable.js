d4555ec53a5f3bbc1b45d78309b9c1f2958bfe6b
Masum Ahmed <creatormasum@gmail.com>
2022-08-01 03:00:26 +0600

    hello first

---
+++ variable.js
@@ -0,0 +1,18 @@
+// Vary + Able 
+// String 
+// Numeric 
+
+
+var price = 13;
+var age = 17;
+var temparature = 38;
+var weight = 37;
+var money = 4000;
+var sex = 999;
+
+var person = "sodor uddin";
+var location = "andork killa bandorbon";
+var special = 'alia bhatt';
+
+
+
