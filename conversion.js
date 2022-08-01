21d899c1e01c41a80e108255457ddf2ed3a749c6
Masum Ahmed <creatormasum@gmail.com>
2022-08-01 11:07:42 +0600

    conversion string number parseInt parseFloat

---
+++ conversion.js
@@ -0,0 +1,60 @@
+var price1 = 31;
+var price2 = 10;
+var totalPrice = price1 + price2
+// console.log(totalPrice) 
+
+
+var firstName = 'Masum';
+var lastName = 'Ahmed';
+var fullName = firstName + lastName;
+// console.log(fullName); 
+
+
+var firstName = 'Masum';
+var lastName = 'Ahmed';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName); 
+
+
+var price1 = '31';
+var price2 = '10';
+var totalPrice = price1 + price2
+// console.log(totalPrice)
+
+
+var price1 = '31';
+var price2 = 10;
+var totalPrice = price1 + price2
+// console.log(totalPrice)
+
+
+//convert string to number
+
+console.log(price2)
+price1 = parseInt(price1)
+// console.log(price1) 
+
+
+
+var gpa = '3.41';
+var gpaNumber = parseInt(gpa);
+// console.log = (gpaNumber);
+
+
+
+
+
+var gpa = '3.41';
+var gpaNumber = parseInt(gpa);
+console.log(gpaNumber);
+
+
+var gpa = '4.50';
+var gpaNumber = parseInt(gpa);
+// console.log(gpaNumber); 
+
+
+var gpa = '3.41';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
+
